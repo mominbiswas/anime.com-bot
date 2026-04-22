0cb66fc0edbfb5e8e39f3623819bdc84b7596712
Shomik <138145411+mominbiswas@users.noreply.github.com>
2026-04-22 13:42:49 +0600

    Update badgeStrip.js

--- src/badgeStrip.js
+++ src/badgeStrip.js
@@ -6,28 +6,93 @@ const PADDING_X = 6;
 const PADDING_Y = 6;
 const ICON_SIZE = 25;
 const MAX_PER_ROW = 8;
-const LABEL_HEIGHT = 18;
+const LABEL_HEIGHT = 16;
 const LABEL_GAP = 4;
 const LABEL_COLOR = "#ffffff";
-const LABEL_SCALE = 0.82;
-const BADGES_WORDMARK_WIDTH = 72;
-const BADGES_WORDMARK = `
-  <g fill="${LABEL_COLOR}">
-    <path d="M1 1h7.6c3.2 0 5.3 1.7 5.3 4.3 0 1.8-.9 3.1-2.5 3.7 2 .5 3.2 2 3.2 4.2 0 3.1-2.3 5-6.1 5H1V1zm4 3.1v3.6h3.2c1.2 0 1.9-.7 1.9-1.8s-.7-1.8-1.9-1.8H5zm0 6.6v4h3.9c1.4 0 2.2-.8 2.2-2s-.8-2-2.2-2H5z"/>
-    <path d="M23.7 18.6c-3.2 0-5.3-2.1-5.3-5.3s2.2-5.4 5.2-5.4c2.8 0 4.8 1.9 4.8 4.7 0 .5 0 .9-.1 1.2h-6c.2 1.3 1.1 2 2.4 2 1 0 1.8-.4 2.7-1.2l2 2c-1.2 1.3-3 2-5.7 2zm-1.3-6.8h2.4c-.1-1-.7-1.7-1.6-1.7-.8 0-1.5.6-1.7 1.7z"/>
-    <path d="M38.2 18.2v-1.1c-.7.9-1.7 1.4-3.1 1.4-2.8 0-4.8-2.2-4.8-5.3 0-3 1.9-5.2 4.8-5.2 1.3 0 2.4.5 3.1 1.4V1h3.8v17.2h-3.8zm-2.1-3c1.2 0 2.1-.9 2.1-2.1s-.9-2.1-2.1-2.1-2.1.9-2.1 2.1.9 2.1 2.1 2.1z"/>
-    <path d="M49.6 22.4c-2.7 0-4.7-.8-6.1-2.2l2.1-2.4c1 1 2.2 1.5 3.7 1.5 1.8 0 2.8-.9 2.8-2.5v-.7c-.8.9-1.8 1.4-3.2 1.4-2.8 0-4.9-2-4.9-4.9S46.1 7.8 49 7.8c1.5 0 2.5.5 3.2 1.4V8h3.8v8.5c0 3.8-2.4 5.9-6.4 5.9zm.5-8.2c1.2 0 2.1-.7 2.1-1.8s-.9-1.8-2.1-1.8-2.1.7-2.1 1.8.9 1.8 2.1 1.8z"/>
-    <path d="M63.5 18.6c-3.2 0-5.3-2.1-5.3-5.3s2.2-5.4 5.2-5.4c2.8 0 4.8 1.9 4.8 4.7 0 .5 0 .9-.1 1.2h-6c.2 1.3 1.1 2 2.4 2 1 0 1.8-.4 2.7-1.2l2 2c-1.2 1.3-3 2-5.7 2zm-1.3-6.8h2.4c-.1-1-.7-1.7-1.6-1.7-.8 0-1.5.6-1.7 1.7z"/>
-    <path d="M73.9 18.5c-1.8 0-3.8-.5-5.4-1.7l1.5-2.4c1.3.9 2.8 1.4 4 1.4.7 0 1-.2 1-.5 0-.4-.5-.5-1.9-.8-2.5-.5-4.1-1.2-4.1-3.3 0-2.2 1.8-3.4 4.3-3.4 1.8 0 3.4.5 4.8 1.4l-1.4 2.5c-1.2-.7-2.5-1.1-3.5-1.1-.6 0-.9.2-.9.5 0 .4.5.5 1.8.8 2.7.5 4.2 1.4 4.2 3.4 0 2.3-1.8 3.3-4.4 3.3z"/>
-  </g>
-`;
+const PIXEL_SIZE = 2;
+const LETTER_SPACING = 2;
+const BADGES_GLYPHS = {
+  B: [
+    "11110",
+    "10001",
+    "10001",
+    "11110",
+    "10001",
+    "10001",
+    "11110"
+  ],
+  a: [
+    "00000",
+    "01110",
+    "00001",
+    "01111",
+    "10001",
+    "10011",
+    "01101"
+  ],
+  d: [
+    "00001",
+    "00001",
+    "01111",
+    "10001",
+    "10001",
+    "10011",
+    "01101"
+  ],
+  g: [
+    "00000",
+    "01101",
+    "10011",
+    "10001",
+    "01111",
+    "00001",
+    "01110"
+  ],
+  e: [
+    "00000",
+    "01110",
+    "10001",
+    "11111",
+    "10000",
+    "10001",
+    "01110"
+  ],
+  s: [
+    "00000",
+    "01111",
+    "10000",
+    "01110",
+    "00001",
+    "00001",
+    "11110"
+  ]
+};
 
 function buildLabelSvg(width) {
+  let x = PADDING_X;
+  const rects = [];
+
+  for (const character of "Badges") {
+    const glyph = BADGES_GLYPHS[character];
+
+    for (let row = 0; row < glyph.length; row += 1) {
+      for (let column = 0; column < glyph[row].length; column += 1) {
+        if (glyph[row][column] !== "1") {
+          continue;
+        }
+
+        rects.push(
+          `<rect x="${x + column * PIXEL_SIZE}" y="${1 + row * PIXEL_SIZE}" width="${PIXEL_SIZE}" height="${PIXEL_SIZE}" fill="${LABEL_COLOR}" />`
+        );
+      }
+    }
+
+    x += glyph[0].length * PIXEL_SIZE + LETTER_SPACING;
+  }
+
   return Buffer.from(`
     <svg width="${width}" height="${LABEL_HEIGHT}" xmlns="http://www.w3.org/2000/svg">
-      <g transform="translate(${PADDING_X} 1) scale(${LABEL_SCALE})">
-        ${BADGES_WORDMARK}
-      </g>
+      ${rects.join("")}
     </svg>
   `);
 }
