66c3870d0fb4f5726980bf088e8c182cf561da22
Shomik <138145411+mominbiswas@users.noreply.github.com>
2026-04-22 08:36:19 +0600

    Delete src directory

--- src/badgeAssets.js
+++
@@ -1,9 +0,0 @@
-const BADGE_ASSET_BASE_URL = "https://animedotcom.b-cdn.net/user-badges";
-
-export function getBadgeIconUrl(key) {
-  return `${BADGE_ASSET_BASE_URL}/icon-filled/${key}.png`;
-}
-
-export function getBadgeImageUrl(key) {
-  return `${BADGE_ASSET_BASE_URL}/large-filled/${key}.png`;
-}
